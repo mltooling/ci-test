4d69f02e9c7ee8e80c691a5c835c7598d1fd0542
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2020-09-22 18:30:29 +0200

    Add more tests for workspace: tool accessibility and ssh connection setup

--- resources/test.py
+++ resources/test.py
@@ -1,6 +1,9 @@
 import unittest
 import requests
 
+from subprocess import run, PIPE
+
+import re
 class TestStringMethods(unittest.TestCase):
 
     def test_healthy(self):
@@ -21,6 +24,36 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
+
+    def test_tool_access(self):
+        # Test whether tools are accessible
+        result = requests.get('http://ci-test-workspace:8091/tools/vnc/?password=vncpassword')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('<title>Desktop VNC</title>', result.text)
+
+        result = requests.get('http://ci-test-workspace:8091/tools/vscode/')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('Microsoft Corporation', result.text)
+
+        result = requests.get('http://ci-test-workspace:8080/tooling/ssh/setup-command?origin=http://localhost:8080')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('/bin/bash', result.text)
+        ssh_script_runner_regex = r'^\/bin\/bash <\(curl -s --insecure "(http:\/\/ci-test-workspace:8091\/shared\/ssh\/setup\?token=[a-z0-9]+&host=ci-test-workspace&port=8091"\))$'
+        pattern = re.compile(ssh_script_runner_regex)
+        match = pattern.match(result.text)
+        self.assertIsNotNone(match)
+        #self.assertRegex(result.text, ssh_script_runner_regex)
+        
+        # Execute the ssh setup script and automatically pass an ssh connection name to the script
+        script_url = match.groups()[0]
+        r = requests.get(script_url)
+        with open('/setup-ssh.sh', 'w') as f:
+            f.write(r.text)
+            # r can now be executed
+        ssh_connection_name = 'test'
+        completed_process = run(['/bin/bash -c "/setup-ssh.sh"'], input=ssh_connection_name, encoding='ascii', shell=True, stdout=PIPE, stderr=PIPE)
+        output = completed_process.stdout
+        self.assertIn('Connection successful!', output)
 
 if __name__ == '__main__':
     unittest.main()
