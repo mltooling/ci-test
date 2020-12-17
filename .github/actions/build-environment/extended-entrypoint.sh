a98048b0977720910b397f921ebe37df8ab14499
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2020-12-17 17:15:17 +0100

    Check docker containers in GitHub Actions

---
+++ .github/actions/build-environment/extended-entrypoint.sh
@@ -0,0 +1,17 @@
+echo "List Docker containers"
+docker ps -a
+
+echo "Inspect Docker container"
+docker inspect $(hostname)
+
+# Thereby, you can reuse the existing implementation:
+/bin/bash /entrypoint.sh "$@"
+# Save the exit code of the previous command
+exit_code=$?
+
+echo "Cleanup Phase"
+
+# TODO: Do additional cleanup
+
+# Exit the script with the exit code of the actual entrypoint execution
+exit $exit_code
